e61a068b27ebb12a05eb88d0b1d1c01b3257caf0
sduprey <stefan.duprey@yahoo.fr>
2018-04-23 14:40:44 +0200

    tes

--- contracts/StrategyManager.sol
+++ contracts/StrategyManager.sol
@@ -3,37 +3,40 @@ pragma solidity ^0.4.18;
 contract StrategyManager{
 
     address public owner;
-    bytes10 public name;      //the name of the strategy
-    bytes10[] strategies;
+    string public name;      //the name of the strategy
+    string[] strategies;
 
     //mapping(bytes15 => StrategyPublisher) public stratRegistry;
-    //mapping(bytes10 => address) public stratRegistry;
+    mapping(address => string) public stratRegistry;
 
-    event newRegister(bytes10 name, address stratAddress);
-
+    event newRegister(string name, address stratAddress);
 
     modifier onlyOwner(){
         require(msg.sender==owner);
         _;
     }
 
-    function StrategyManager(bytes10 _managerName){
+    function StrategyManager(string _managerName){
       name = _managerName;
     }
 
-    function registerNewStrategy(address _newAddress, bytes10 _stratName) onlyOwner {
-      //stratRegistry[_stratName] = _newAddress;
-      //strategies.push(_stratName);
-      //newRegister(_stratName, _newAddress);
+    function registerNewStrategy(address _newAddress, string _stratName) onlyOwner {
+      stratRegistry[_newAddress] = _stratName;
+      strategies.push(_stratName);
+      newRegister(_stratName, _newAddress);
     }
 
     function getNumberRegisteredStrategies() public constant returns (uint) {
       return(strategies.length);
     }
 
-//    function getStrategyAddress(bytes10 _stratName) public constant returns (address){
-//      address myAddress = stratRegistry[_stratName];
-//      return(myAddress);
-//    }
+    function getRegisteredStrategyName(uint index) public constant returns (string) {
+      return(strategies[index]);
+    }
+
+    function getStrategyAddress(address _stratAddress) public constant returns (string){
+      string stratName = stratRegistry[_stratAddress];
+      return(stratName);
+    }
 
 }
