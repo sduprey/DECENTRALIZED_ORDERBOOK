9222dc0e948913aed98d109a763ffbd9525fe43e
sduprey <stefan.duprey@yahoo.fr>
2018-04-23 15:45:40 +0200

    strat manager

--- contracts/StrategyManager.sol
+++ contracts/StrategyManager.sol
@@ -3,40 +3,43 @@ pragma solidity ^0.4.18;
 contract StrategyManager{
 
     address public owner;
-    string public name;      //the name of the strategy
-    string[] strategies;
+    bytes15 public name;      //the name of the strategy
+    bytes15[] strategies;
 
+    //mapping(string => StrategyPublisher) public stratRegistry;
     //mapping(bytes15 => StrategyPublisher) public stratRegistry;
-    mapping(address => string) public stratRegistry;
+    mapping(bytes15 => address) public stratRegistry;
 
-    event newRegister(string name, address stratAddress);
+    event newRegister(bytes15 name, address stratAddress);
 
     modifier onlyOwner(){
         require(msg.sender==owner);
         _;
     }
 
-    function StrategyManager(string _managerName){
+    function StrategyManager(bytes15 _managerName){
       name = _managerName;
+      owner = msg.sender;
     }
 
-    function registerStrategy(string _stratName,address _newAddress) onlyOwner {
+    function registerStrategy(bytes15 _stratName,address _newAddress) onlyOwner {
       //stratRegistry[_newAddress] = _stratName;
-      //strategies.push(_stratName);
-      //newRegister(_stratName, _newAddress);
+      stratRegistry[_stratName] = _newAddress;
+      strategies.push(_stratName);
+      newRegister(_stratName, _newAddress);
     }
 
     function getNumberRegisteredStrategies() public constant returns (uint) {
       return(strategies.length);
     }
 
-    function getRegisteredStrategyName(uint index) public constant returns (string) {
+    function getRegisteredStrategyName(uint index) public constant returns (bytes15) {
       return(strategies[index]);
     }
 
-    function getStrategyAddress(address _stratAddress) public constant returns (string){
-      string stratName = stratRegistry[_stratAddress];
-      return(stratName);
+    function getStrategyAddress(bytes15 _stratName) public constant returns (address){
+      address _stratAddress = stratRegistry[_stratName];
+      return(_stratAddress);
     }
 
 }
