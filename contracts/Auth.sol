43ced5bb4bd3a0f067d3fbeee6a578135ec633ac
sduprey <stefan.duprey@yahoo.fr>
2018-04-27 15:50:28 +0200

    verify function

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -9,11 +9,17 @@ contract Auth {
     }
 
     function verify(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) constant returns (address) {
+	bytes32 _messageHash = sha3(_message);
+        address signer = ecrecover(_messageHash, _v, _r, _s);
+        return signer;
+    }
+
+    function verifyHash(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) constant returns (address) {
         address signer = ecrecover(_message, _v, _r, _s);
         return signer;
     }
 
-    function verifyWithPrefix(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) constant returns (address) {
+    function verifyHashWithPrefix(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) constant returns (address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHash = sha3(prefix, _message);
         address signer = ecrecover(prefixedHash, _v, _r, _s);
