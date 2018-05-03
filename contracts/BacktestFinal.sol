0010a275dfd2b2758eb2df2c5c80299a92949535
sduprey <stefan.duprey@yahoo.fr>
2018-05-03 09:57:55 +0200

    test

--- contracts/BacktestFinal.sol
+++ contracts/BacktestFinal.sol
@@ -5,19 +5,17 @@ contract BacktestFinal {
     bytes15 public name;
 
     struct StrategyExplicitexplicitState {
-      uint32 date;
+      uint64 date;
       int8 position;
       int256 variation;
-      uint32 price;
+      uint64 price;
       int8 category;
     }
 
     StrategyExplicitexplicitState explicitState;
 
-    event newRecord(bytes15 indexed name, uint32 indexed date, int256 stratVariation, uint48 stratPrice, int8 position,int8 category);
+    event newRecord(bytes15 indexed name, uint64 indexed date, int256 stratVariation, uint64 stratPrice, int8 position,int8 category);
 
-    event absoluteVariation(int256 stratVariation);
-    event relativeVariation(int256 stratVariation);
 
     function BacktestFinal(){
         name = "Multi 1.0";
@@ -25,21 +23,21 @@ contract BacktestFinal {
     }
     // that backtesting function requires that the strategy explicitState be at the date matching
     // the beginning of the array date
-    function batchbacktest(uint32[]dates, uint32[] prices, int8[] positions) public {
+    function batchbacktest(uint64[]dates, uint64[] prices, int8[] positions) public {
       uint batchlength = positions.length;
       require(dates.length==batchlength && prices.length==batchlength);
 
-      uint32 strategyInitPrice = explicitState.price;
+      uint64 strategyInitPrice = explicitState.price;
 
-      uint32 strategyPrice = libbatchbacktest(name,strategyInitPrice,dates,prices,positions);
+      uint64 strategyPrice = libbatchbacktest(name,strategyInitPrice,dates,prices,positions);
 
       explicitState.position = positions[batchlength-1];
       explicitState.date = dates[batchlength-1];
       explicitState.price = strategyPrice;
   	}
 
-    function libbatchbacktest(bytes15 name, uint32 strategyInitPrice, uint32[]dates, uint32[] prices, int8[] positions) public returns (uint32) {
-         uint32 strategyPrice = strategyInitPrice;
+    function libbatchbacktest(bytes15 name, uint64 strategyInitPrice, uint64[]dates, uint64[] prices, int8[] positions) public returns (uint64) {
+         uint64 strategyPrice = strategyInitPrice;
 
          for(uint8 i=1; i<dates.length; i++){
 
@@ -60,17 +58,15 @@ Compute the variation of price of index according to the last position EOD
 ----Return:
        (pxCur/pxPre)^(lastPosit)-1
 -------------------------------------------------------------------------*/
-  function calVariation(uint32 pxPre, uint32 pxCur, int8 lastPosit) internal returns (int256 ){
+  function calVariation(uint64 pxPre, uint64 pxCur, int8 lastPosit) internal returns (int256 ){
      int256 variation;
      if(lastPosit==0){
          variation = 0;
      }
      else{
          variation = int256(pxCur) - int256(pxPre);
-         emit absoluteVariation(variation);
-         variation *= 1e64;
+         variation *= 1e32;
          variation /=  pxPre;
-         emit relativeVariation(variation);
          if(lastPosit==-1){
              variation *=-1;
          }
@@ -86,12 +82,12 @@ Compute the variation of price of index according to the last position EOD
   ----Return :
      lastValue*(1+variation)
   --------------------------------------------------------------------------*/
-  function calValue(int256 variation, uint32 lastValue) internal returns (uint32){
+  function calValue(int256 variation, uint64 lastValue) internal returns (uint64){
      if(variation==0) return lastValue;
      else {
          int256 delta = int256(lastValue) * variation;
-         delta/=1e64;
-         return uint32(int256(lastValue) +delta);
+         delta/=1e32;
+         return uint64(int256(lastValue) +delta);
      }
   }
 
